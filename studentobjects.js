24d96f985c9237bb10cc55a8ec2e69dea544ad51
Kirtishil Patil <kirtishil358@gmail.com>
2020-12-15 20:00:31 +0530

    Delete studentobjects.js

--- studentobjects.js
+++
@@ -1,9 +0,0 @@
-var studentList = [ {Name:'Kirtishil'},
-                    {RollNO:123},
-                    {Phone:9623562228},
-                    {Department:"Comp"},
-                    {Hobbies:['Reading', 'Music']}];
-
-// console.log(studentList);
-
-studentList.forEach(element => console.log(element));
