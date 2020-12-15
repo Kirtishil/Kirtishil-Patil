f57688ec41363d58cb079f4c203c98e8b7e6c2ec
Kirtishil Patil <kirtishil358@gmail.com>
2020-12-15 16:56:02 +0530

    Add files via upload

---
+++ studentobjects.js
@@ -0,0 +1,9 @@
+var studentList = [ {Name:'Kirtishil'},
+                    {RollNO:123},
+                    {Phone:9623562228},
+                    {Department:"Comp"},
+                    {Hobbies:['Reading', 'Music']}];
+
+// console.log(studentList);
+
+studentList.forEach(element => console.log(element));
