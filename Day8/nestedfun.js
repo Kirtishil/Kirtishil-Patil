b6e983d518d049d35cdd79c64050a33afa5e73eb
Kirtishil Patil <kirtishil358@gmail.com>
2020-12-16 14:30:42 +0530

    Add files via upload

---
+++ Day8/nestedfun.js
@@ -0,0 +1,8 @@
+//nested
+var showProduct=function(name,brand,cost){
+    return function(discount){
+        console.log('Name:=',name,'Brand:=',brand,'Cost:=',cost,'Discount:=',discount);
+    }
+}
+var print=showProduct('OnePlus','OnePlus8T',49000);
+print(2000);
